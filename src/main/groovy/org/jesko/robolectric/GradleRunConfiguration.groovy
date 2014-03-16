8b72b8dfe83c9b5edefefd691a2ae07d8a6c791e
ben <jeske717@gmail.com>
2014-03-15 23:05:30 -0500

    made the toXml method work when the defaults argument is unparseable by working with a reasonable default

--- src/main/groovy/org/jesko/robolectric/GradleRunConfiguration.groovy
+++ src/main/groovy/org/jesko/robolectric/GradleRunConfiguration.groovy
@@ -1,5 +1,7 @@
 package org.jesko.robolectric
 
+import org.xml.sax.SAXParseException
+
 class GradleRunConfiguration {
 
     String name
@@ -11,7 +13,12 @@ class GradleRunConfiguration {
     }
 
     def toXml(defaults) {
-        def defaultsCopy = new XmlParser().parseText(XmlUtil.writeXmlToString(defaults))
+        def defaultsCopy
+        try {
+            defaultsCopy = new XmlParser().parseText(XmlUtil.writeXmlToString(defaults))
+        } catch(SAXParseException ignored) {
+            defaultsCopy = getGradleRunConfigDefaultXml()
+        }
         defaultsCopy.attributes()['default'] = 'false'
         defaultsCopy.attributes()['name'] = name
         defaultsCopy.attributes()['factoryName'] = 'Gradle'
@@ -25,6 +32,28 @@ class GradleRunConfiguration {
         externalSystemSettings.find { it.attributes()['name'] == 'externalProjectPath' }.attributes()['value'] = '$PROJECT_DIR$'
 
         return defaultsCopy
+    }
+
+    static def getGradleRunConfigDefaultXml() {
+        return new XmlParser().parseText("""
+         <configuration default="false" name="" type="GradleRunConfiguration" factoryName="Gradle">
+          <ExternalSystemSettings>
+            <option name="externalProjectPath" value="\\\$PROJECT_DIR\\\$" />
+            <option name="externalSystemIdString" value="GRADLE" />
+            <option name="scriptParameters" value="" />
+            <option name="taskDescriptions">
+              <list />
+            </option>
+            <option name="taskNames">
+              <list />
+            </option>
+            <option name="vmOptions" value="" />
+          </ExternalSystemSettings>
+          <RunnerSettings RunnerId="ExternalSystemTaskRunner" />
+          <ConfigurationWrapper RunnerId="ExternalSystemTaskRunner" />
+          <method />
+        </configuration>
+        """)
     }
 
 }
